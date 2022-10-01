768a1fc9f9956a3ce019d0f68ac4419c40bfcafd
dMaryuma <68241370+dMaryuma@users.noreply.github.com>
2022-10-01 08:41:08 +0000

    Add files via upload

---
+++ Check_Acl.ps1
@@ -0,0 +1,15 @@
+﻿$volPath = "\\192.168.0.130\volmulti"
+$folders = Get-ChildItem $volPath
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("DEMO\Domain Admins","FullControl","Allow")
+$arrayFullPath = @()
+$arrayFolderName = @()
+foreach ($f in $folders){
+    $acl = Get-Acl $f.FullName
+    if ($acl.Access.Count -eq 1){
+        if ($AccessRule.IdentityReference -in $acl.Access[0].IdentityReference){
+            Write-Host "$($f.FullName) only have $($AccessRule.IdentityReference) ACL"
+            $arrayFullPath += $f.FullName
+            $arrayFolderName += $f.Name
+        }
+    }
+}
